3b11875ac86e2bbab7a4e182dbc3b0481a0e353a
Deeban Ramalingam <rdeeban@gmail.com>
2023-08-26 17:41:01 -0700

    Worked on Programming Interview Problem 1

---
+++ src/main/ProgrammingInterviewProblems.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.ArrayList;
+
+public class ProgrammingInterviewProblems {
+
+    /**
+     * Return the missing number in a given integer array of 1 to 100 (sorted)
+     */
+    public static int findMissingNumber(ArrayList<Integer> arr) {
+        boolean isNumberMissing = false;
+        for (int i = 0; i < 100; i ++) {
+            if (i == 99 && arr.size() == 99) {
+                isNumberMissing = true;
+                break;
+            }
+
+            if (i + 1 < arr.get(i))
+                isNumberMissing = true;
+        }
+
+        if (!isNumberMissing) throw new IllegalArgumentException("Invalid input: there are no numbers missing.");
+
+        for (int i = 0; i < 99; i ++)
+            if (i + 1 < arr.get(i))
+                return i + 1;
+
+        return 100;
+    }
+}
---
+++ src/test/TestProgrammingInterviewProblems.java
@@ -0,0 +1,57 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import main.ProgrammingInterviewProblems;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TestProgrammingInterviewProblems {
+
+    private static ArrayList<Integer> prob1Arr;
+
+    @Test
+    public void testProblem1InvalidInputNoNumberMissingCase() {
+        prob1Arr = new ArrayList<>();
+        for (int i = 1; i <= 100; i ++)
+            prob1Arr.add(i);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> ProgrammingInterviewProblems.findMissingNumber(prob1Arr));
+        assertEquals("Invalid input: there are no numbers missing.", exception.getMessage());
+    }
+
+    @Test
+    public void testProblem1FirstNumberMissingCase() {
+        prob1Arr = new ArrayList<>();
+        for (int i = 1; i <= 100; i ++)
+            if (i != 1)
+                prob1Arr.add(i);
+
+        int missingNumber = ProgrammingInterviewProblems.findMissingNumber(prob1Arr);
+        assertEquals(1, missingNumber);
+    }
+
+    @Test
+    public void testProblem1LastNumberMissingCase() {
+        prob1Arr = new ArrayList<>();
+        for (int i = 1; i <= 100; i ++)
+            if (i != 100)
+                prob1Arr.add(i);
+
+        int missingNumber = ProgrammingInterviewProblems.findMissingNumber(prob1Arr);
+        assertEquals(100, missingNumber);
+    }
+
+    @Test
+    public void testProblem1MissingNumberGeneralCase() {
+        prob1Arr = new ArrayList<>();
+        for (int i = 1; i <= 100; i ++)
+            if (i != 2)
+                prob1Arr.add(i);
+
+        int missingNumber = ProgrammingInterviewProblems.findMissingNumber(prob1Arr);
+        assertEquals(2, missingNumber);
+    }
+}
