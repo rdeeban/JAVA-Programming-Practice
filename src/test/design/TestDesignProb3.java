45b4a46713a90bc413c142f92bb7a4afa54f8f2d
Deeban Ramalingam <rdeeban@gmail.com>
2023-09-16 15:04:24 -0700

    Worked on Traffic problem

--- src/test/design/TestDesignProb3.java
+++ src/test/design/TestDesignProb3.java
@@ -18,13 +18,38 @@ public class TestDesignProb3 {
         control.put(Light.ORANGE, 1);
         TrafficLight trafficLight = new TrafficLight(control, Light.RED);
         trafficLight.operateFor(1);
-        Light light = trafficLight.getLight();
+        Light light = trafficLight.getCurrentLight();
         Assert.assertEquals(Light.GREEN, light);
         trafficLight.operateFor(1);
-        light = trafficLight.getLight();
+        light = trafficLight.getCurrentLight();
         Assert.assertEquals(Light.ORANGE, light);
         trafficLight.operateFor(1);
-        light = trafficLight.getLight();
+        light = trafficLight.getCurrentLight();
         Assert.assertEquals(Light.RED, light);
+
+        trafficLight = new TrafficLight(control, Light.RED);
+        trafficLight.operateFor(2);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.ORANGE, light);
+        trafficLight.operateFor(1);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.RED, light);
+        trafficLight.operateFor(1);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.GREEN, light);
+
+        control.put(Light.RED, 2);
+        control.put(Light.GREEN, 1);
+        control.put(Light.ORANGE, 1);
+        trafficLight = new TrafficLight(control, Light.RED);
+        trafficLight.operateFor(1);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.RED, light);
+        trafficLight.operateFor(1);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.GREEN, light);
+        trafficLight.operateFor(1);
+        light = trafficLight.getCurrentLight();
+        Assert.assertEquals(Light.ORANGE, light);
     }
 }
--- src/trafficcontrol/TrafficLight.java
+++ src/trafficcontrol/TrafficLight.java
@@ -1,5 +1,6 @@
 package trafficcontrol;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TrafficLight {
@@ -7,22 +8,31 @@ public class TrafficLight {
     public enum Light {RED, GREEN, ORANGE}
 
     private final Map<Light, Integer> control;
-    private Light light;
-    private int operationTime;
+    private Light currentLight;
+    private final Map<Light, Integer> timeSpentAtLight;
 
+    /**
+     * Constructs a Traffic Light object
+     *
+     * @param control traffic light control
+     * @param light starting light
+     */
     public TrafficLight(Map<Light, Integer> control, Light light) {
         this.control = control;
-        this.light = light;
-        operationTime = 0;
+        currentLight = light;
+        timeSpentAtLight = new HashMap<>();
+        timeSpentAtLight.put(Light.RED, 0);
+        timeSpentAtLight.put(Light.GREEN, 0);
+        timeSpentAtLight.put(Light.ORANGE, 0);
     }
 
     /**
      * Returns the color of the traffic light.
      *
-     * @return  Red, green, or orange
+     * @return  red, green, or orange
      */
-    public Light getLight() {
-        return light;
+    public Light getCurrentLight() {
+        return currentLight;
     }
 
     /**
@@ -31,15 +41,24 @@ public class TrafficLight {
      * @param   duration    how long to operate the traffic light for
      */
     public void operateFor(int duration) {
-        operationTime += duration;
-        if (light == Light.RED && operationTime == duration) {
-            light = Light.GREEN;
-        }
-        else if (light == Light.GREEN && operationTime == duration + duration) {
-            light = Light.ORANGE;
-        }
-        else if (light == Light.ORANGE && operationTime == duration + duration + duration) {
-            light = Light.RED;
+        int previousTimeSpentAtCurrentLight = timeSpentAtLight.get(currentLight);
+        int currentTimeSpentAtCurrentLight = previousTimeSpentAtCurrentLight + duration;
+        timeSpentAtLight.put(currentLight, currentTimeSpentAtCurrentLight);
+        int timeNeededToSpendAtCurrentLight = control.get(currentLight);
+        while(currentTimeSpentAtCurrentLight >= timeNeededToSpendAtCurrentLight) {
+            timeSpentAtLight.put(currentLight, 0);
+            if (currentLight == Light.RED) {
+                currentLight = Light.GREEN;
+            } else if (currentLight == Light.GREEN) {
+                currentLight = Light.ORANGE;
+            } else if (currentLight == Light.ORANGE) {
+                currentLight = Light.RED;
+            }
+
+            currentTimeSpentAtCurrentLight -= timeNeededToSpendAtCurrentLight;
+            timeSpentAtLight.put(currentLight, currentTimeSpentAtCurrentLight);
+            currentTimeSpentAtCurrentLight = timeSpentAtLight.get(currentLight);
+            timeNeededToSpendAtCurrentLight = control.get(currentLight);
         }
     }
 }
